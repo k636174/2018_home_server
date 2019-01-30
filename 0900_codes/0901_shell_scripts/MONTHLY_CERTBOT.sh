87569d2eed0d6525552b9e7ff655e2703acd6755
Shinji Kaneko <shinji@k636174.net>
2019-01-31 00:34:27 +0900

    change MONTHLY_CERTBOT.sh

--- 0900_codes/0901_shell_scripts/MONTHLY_CERTBOT.sh
+++ 0900_codes/0901_shell_scripts/MONTHLY_CERTBOT.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-systemctl stop httpd
-certbot certonly --standalone -d sar.k636174.net -m shinji@k636174.net --agree-tos
-certbot certonly --standalone -d imk.k636174.net -m shinji@k636174.net --agree-tos
-certbot certonly --standalone -d mfr.k636174.net -m shinji@k636174.net --agree-tos
-certbot certonly --standalone -d phpmyadmin.k636174.net -m shinji@k636174.net --agree-tos
-certbot certonly --standalone -d sar.k636174.net -m shinji@k636174.net --agree-tos
-certbot certonly --standalone -d zbr.k636174.net -m shinji@k636174.net --agree-tos
-systemctl start httpd
+certbot renew --pre-hook "systemctl stop httpd" --post-hook  "systemctl start httpd"
