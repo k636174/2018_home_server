1eacc88e5b5e994010557cab1a29403cb757938e
Shinji Kaneko <shinji@k636174.net>
2019-02-15 00:12:35 +0900

    change DAILY_BACKUP_SCRIPT_COMMON.sh #1

--- 0900_codes/0901_shell_scripts/DAILY_BACKUP_SCRIPT_COMMON.sh
+++ 0900_codes/0901_shell_scripts/DAILY_BACKUP_SCRIPT_COMMON.sh
@@ -8,18 +8,15 @@ cd $SCRIPT_DIR
 . ./CONFIG.txt
 
 # COPY CRON
-if [ -e /var/spool/cron/ ]; then
+if [ -e "/var/spool/cron/" ]; then
   #CentOS
-  $CRON_SOURCE_DIR=/var/spool/cron/
-elif [ -e /usr/lib/cron/tabs/ ]; then
+  CRON_SOURCE_DIR=/var/spool/cron/
+elif [ -e "/usr/lib/cron/tabs/" ]; then
   #Mac
-  $CRON_SOURCE_DIR=/usr/lib/cron/tabs/
+  CRON_SOURCE_DIR=/usr/lib/cron/tabs/
 else
     exit
 fi
-
-
-ls -alh $CRON_SOURCE_DIR
 
 mkdir -p $DEST_DIR/cron/
 cp -f $CRON_SOURCE_DIR* $DEST_DIR/cron/
@@ -29,7 +26,8 @@ cd $DEST_DIR
 cd ../../
 git pull
 cd $DEST_DIR
-git add -A
+git add -n *
+exit
 git commit -m "This push by cron(`hostname`)"
 git ../../
 git push
